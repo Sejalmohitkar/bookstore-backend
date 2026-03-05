f792009b0a9629e89298f76a84646245d1dc4e15
Sejalmohitkar <sejalmohitkar2002@gmail.com>
2026-03-05 11:50:51 +0530

    add root route

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send("📚 Bookstore API is running successfully");
+});
 // Routes
 app.use('/api/books', bookRoutes);
 
